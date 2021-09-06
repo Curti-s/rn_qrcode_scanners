9cfaa1a5776da7a8c93c1dd38d36d3ba04b318fa
k1r1m1 <kirimi@paygoenergy.co>
2021-09-07 00:21:17 +0300

    wip: attempt synchronous implementation of mlkit

--- android/app/src/main/java/com/test_vision_camera/QRcodeFrameProcessor.java
+++ android/app/src/main/java/com/test_vision_camera/QRcodeFrameProcessor.java
@@ -6,6 +6,12 @@ import android.media.Image;
 import android.annotation.SuppressLint;
 import androidx.camera.core.ImageProxy;
 import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeArray;
+import com.facebook.react.bridge.WritableNativeMap;
+import com.google.android.gms.tasks.Tasks;
 import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin;
 import com.google.mlkit.vision.barcode.Barcode;
 import com.google.mlkit.vision.barcode.BarcodeScanner;
@@ -24,57 +30,51 @@ public class QRcodeFrameProcessor extends FrameProcessorPlugin {
     Log.d("ExampleFrameProcessor", frame.getWidth() + " x " + frame.getHeight() + " Image with format #" + frame.getFormat() + ". Logging " + params.length + " parameters:");
 
     for (Object param : params) {
-        Log.d("ExampleFrameProcessor", "  -> " + (param == null ? "(null)" : param.toString() + " (" + param.getClass().getName() + ")"));
+      Log.d("ExampleFrameProcessor", "  -> " + (param == null ? "(null)" : param.toString() + " (" + param.getClass().getName() + ")"));
     }
-    
+
     @SuppressLint("UnsafeOptInUsageError")
     Image mediaImage = frame.getImage();
 
+    return scanBarcodes(mediaImage, frame);
+  }
+
+  private WritableNativeArray scanBarcodes(Image mediaImage, ImageProxy frame) {
     if(mediaImage != null) {
       InputImage image = InputImage.fromMediaImage(mediaImage, frame.getImageInfo().getRotationDegrees());
       // start detector options
       BarcodeScannerOptions options = new BarcodeScannerOptions
-        .Builder()
-        .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
-        .build();
+              .Builder()
+              .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+              .build();
       // end detector options
 
       // start get detector
       BarcodeScanner scanner = BarcodeScanning.getClient(options);
       // end get_detector
 
-      try {
-        // start run_detector
-        Task<List<Barcode>>result = scanner.process(image)
-          .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
-            @Override
-            public void onSuccess(List<Barcode> barcodes) {
-              // task completed successfully
-              for (Barcode barcode: barcodes) {
-                Rect bounds = barcode.getBoundingBox();
-                Point[] corners = barcode.getCornerPoints();
-                String rawValue = barcode.getRawValue();
-                int valueType = barcode.getValueType();
+      // start run_detector
+      Task<List<Barcode>> task = scanner.process(image);
 
-                switch(valueType) {
-                  case Barcode.TYPE_URL:
-                    String title = barcode.getUrl().getTitle();
-                    String url = barcode.getUrl().getUrl();
-                    break;
-                }
-              }
-            }
-          })
-        .addOnFailureListener(new OnFailureListener() {
-          @Override
-          public void onFailure(@NonNull Exception e) {
-            // task failed
-            e.printStackTrace();
-          }
-        });
-        // end run_detector
-        return result;
-      } catch(Exception e) {
+      WritableNativeArray array =  new WritableNativeArray();
+      try {
+        List<Barcode> barcodes = Tasks.await(task); // synchronous
+//        this.scannerSuccessListener(task, barcodes)
+
+        WritableMap map = new WritableNativeMap();
+        for(Barcode barcode: barcodes) {
+          Rect bounds = barcode.getBoundingBox();
+          Point[] corners = barcode.getCornerPoints();
+
+          String rawValue = barcode.getRawValue();
+          int valueType = barcode.getValueType();
+
+//          map.putMap();
+        }
+
+        array.pushMap(map);
+        return array;
+      } catch (Exception e) {
         e.printStackTrace();
       }
     }
