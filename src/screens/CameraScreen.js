a641bdb2b28b2ec37f46014f515a1ceda028c011
Shadrack Lilan <shadrack.lilan@paygoenergy.co>
2021-09-07 06:36:34 +0300

    remove option for taking pictures; simplify code + lint

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,66 +1,44 @@
-import React, { PureComponent } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
-export default class CameraScreen extends PureComponent {
-  constructor(props) {
-    super(props);
+export default function CameraScreen({navigation}) {
+  if (!navigation.isFocused()) {
+    return null;
   }
 
-  takePicture = async () => {
-    if(this.camera) {
-      const options = { quality:0.5, base64:true };
-      const data = await this.camera.takePictureAsync(options);
-      console.log('data: ', data.uri);
-    }
-  }
-
-  render() {
-    const { navigation } = this.props;
-
-    if(navigation.isFocused()) {
-      return (
-        <View style={styles.container}>
-          <RNCamera 
-            ref={ref => { this.camera = ref }}
-            style={styles.preview}
-            type={RNCamera.Constants.Type.back}
-            flashMode={RNCamera.Constants.FlashMode.on}
-            androidCameraPermissionOptions={{
-              title: 'Permission to use camera',
-                message: 'Camera permissions required',
-                buttonPositive: 'Ok',
-                buttonNegative: 'Cancel',
-            }}
-            onGoogleVisionBarcodesDetected={({ barcodes }) => {
-              const startTime = Date.now();
-              console.log('barcodes ', JSON.stringify(barcodes));
-              console.log('Elapsed Time: ', Date.now() - startTime, '[ms] ');
-            }}
-            captureAudio={false}
-            />
-        <View style={{ flex:0, flexDirection:'row', justifyContent:'center' }}>
-          <TouchableOpacity 
-            onPress={this.takePicture}
-            style={styles.capture}>
-            <Text style={{ fontSize:14 }}>Snap</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      );
-    }
-  }
+  return (
+    <View style={styles.container}>
+      <RNCamera
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.on}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'Camera permissions required',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        onGoogleVisionBarcodesDetected={({barcodes}) => {
+          const startTime = Date.now();
+          console.log('barcodes ', barcodes.map((i) => i.data));
+          console.log('Elapsed Time: ', Date.now() - startTime, '[ms] ');
+        }}
+        captureAudio={false}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: { 
-    flex:1,
+  container: {
+    flex: 1,
     backgroundColor: 'black',
   },
   preview: {
-    flex:1,
+    flex: 1,
     justifyContent: 'flex-end',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   capture: {
     flex: 0,
@@ -71,4 +49,4 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     margin: 20,
   },
-})
+});
