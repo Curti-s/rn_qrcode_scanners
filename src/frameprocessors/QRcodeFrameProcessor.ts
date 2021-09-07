691f6478fde8d007a6c736a4f792256a7f962991
Shadrack Lilan <shadrack.lilan@paygoenergy.co>
2021-09-07 07:00:44 +0300

    lint fix babel config

--- src/frameprocessors/QRcodeFrameProcessor.ts
+++ src/frameprocessors/QRcodeFrameProcessor.ts
@@ -1,10 +1,8 @@
 /* global __scanQRcodes */
 import type { Frame } from 'react-native-vision-camera';
 
-declare let _WORKLET; true | undefined;
-
 export function scanQRcodes(frame: Frame): string[] {
   'worklet';
-  if (!_WORKLET) throw new Error('QRcodeFrameProcessor must be called from a frame processor');
+  // @ts-ignore
   return __scanQRcodes(frame);
 }
