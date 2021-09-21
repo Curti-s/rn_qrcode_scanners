7c8c51493a154b5ba0a75c3654e614dc10b9a24b
k1r1m1 <kirimi@paygoenergy.co>
2021-09-21 18:07:14 +0300

    RNCamera: add tap to focus

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -5,9 +5,12 @@ import {
   Text,
   Pressable,
   Dimensions,
-  TouchableWithoutFeedback,
 } from 'react-native';
-import {RNCamera} from 'react-native-camera';
+import { RNCamera } from 'react-native-camera';
+import {
+  TapGestureHandler,
+  State,
+} from 'react-native-gesture-handler';
 
 export default class CameraScreen extends Component {
   state = {
@@ -48,6 +51,23 @@ export default class CameraScreen extends Component {
   toggleZoom = () => {
     this.setState({ zoom:this.state.zoom + 0.5 });
     if(this.state.zoom === 1.0) this.setState({ zoom:0 });
+  onTapToFocus = (event) => {
+    const { absoluteX, absoluteY, state, } = event.nativeEvent;
+    const { width: screenWidth, height: screenHeight } = Dimensions.get('screen');
+    const isPortrait = screenHeight > screenWidth;
+
+    let x, y, maxDeltaY;
+    if (state === State.ACTIVE) {
+      if (!isPortrait) {
+        x = absoluteX / screenWidth;
+        y = absoluteY / screenHeight;
+      } else {
+        x = absoluteY / screenHeight;
+        y = -(absoluteX / screenWidth) + 1;
+      }
+      maxDeltaY = screenHeight - 100.0;
+      this.setState({ autoFocusPointOfInterest: { normalised: { x, y }, drawRectPosition: { x: absoluteX, y: absoluteY < maxDeltaY && absoluteY } }, maxDeltaY });
+    }
   }
 
   render() {
@@ -59,63 +79,60 @@ export default class CameraScreen extends Component {
     };
 
     return (
-      <View style={styles.container}>
-        {!!barcodeData && (
-          <Text style={styles.code}>{barcodeData}</Text>
-        )}
-        <RNCamera
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.off}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'Camera permissions required',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          onGoogleVisionBarcodesDetected={this.onRead}
-          captureAudio={false}
-          autoFocus={this.state.autoFocus}
-          autoFocusPointOfInterest={this.state.autoFocusPointOfInterest.normalised}
-          focusDepth={this.state.focusDepth}
-          flashMode={this.state.flash}
-          zoom={this.state.zoom}
-        />
-        <View style={StyleSheet.absoluteFill}>
-          <View style={[styles.autoFocusBox, drawFocusRingPosition]} />
-          <TouchableWithoutFeedback>
-            <View style={{ flex:1 }} />
-          </TouchableWithoutFeedback>
-        </View>
-        <View style={{ bottom:0 }}>
-          <View>
+      <TapGestureHandler onHandlerStateChange={this.onTapToFocus} numberOfTaps={1} maxDeltaY={this.state.maxDeltaY}>
+        <View style={styles.container}>
+          {!!barcodeData && (
+            <Text style={styles.code}>{barcodeData}</Text>
+          )}
+          <RNCamera
+            style={styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.off}
+            androidCameraPermissionOptions={{
+              title: 'Permission to use camera',
+              message: 'Camera permissions required',
+              buttonPositive: 'Ok',
+              buttonNegative: 'Cancel',
+            }}
+            onGoogleVisionBarcodesDetected={this.onRead}
+            captureAudio={false}
+            autoFocus={this.state.autoFocus}
+            autoFocusPointOfInterest={this.state.autoFocusPointOfInterest.normalised}
+            focusDepth={this.state.focusDepth}
+            flashMode={this.state.flash}
+            zoom={this.state.zoom}
+          />
+          <View style={StyleSheet.absoluteFill}>
+            <View style={[styles.autoFocusBox, drawFocusRingPosition]} />
+              <View style={{ flex: 1 }} />
           </View>
-          <View
-            style={{
-              height: 56,
+          <View style={{ bottom: 0 }}>
+            <View>
+            </View>
+            <View
+              style={{
+                height: 56,
                 backgroundColor: 'transparent',
                 flexDirection: 'row',
                 alignSelf: 'flex-end',
-            }}>
+              }}>
               <Pressable
-                style={[styles.btn, { alignSelf:'flex-end'} ]} onPress={this.toggleAutoFocus} disabled={this.state.focusDepth}>
+                style={[styles.btn, { alignSelf: 'flex-end' }]} onPress={this.toggleAutoFocus} disabled={this.state.focusDepth}>
                 <Text style={[styles.txt]}>Auto Focus: {this.state.autoFocus}</Text>
               </Pressable>
               <Pressable
-                style={[styles.btn, { alignSelf:'flex-end'} ]} onPress={this.toggleFocusDepth}>
+                style={[styles.btn, { alignSelf: 'flex-end' }]} onPress={this.toggleFocusDepth}>
                 <Text style={[styles.txt]}>Focus Depth: {this.state.focusDepth}</Text>
               </Pressable>
               <Pressable
-                style={[styles.btn, { alignSelf:'flex-end'} ]} onPress={this.toggleFlashMode}>
-                <Text style={[styles.txt]}>Flash: {this.state.flash}</Text>
-              </Pressable>
-              <Pressable
-                style={[styles.btn, { alignSelf:'flex-end'} ]} onPress={this.toggleZoom}>
+
+                style={[styles.btn, { alignSelf: 'flex-end' }]} onPress={this.toggleZoom}>
                 <Text style={[styles.txt]}>Zoom: {this.state.zoom}</Text>
               </Pressable>
+            </View>
           </View>
         </View>
-      </View>
+      </TapGestureHandler>
     );
   }
 }
